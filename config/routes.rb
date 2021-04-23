9f3143fd449f0c8251dd493d2e352ce184878a3d
Karl Heitmann <khf1988@gmail.com>
2021-04-22 23:18:39 -0400

    navbar agregado

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :videos
   resources :users
 
-  root 'users#index'
+  root 'videos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
