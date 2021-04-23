cc4c136c738cc4c3dd6df8cc50d05fd8a551db17
Karl Heitmann <khf1988@gmail.com>
2021-04-23 00:15:27 -0400

    por fin terminadoooooooo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def number_in_words(n)
+    if n < 1000
+      return n.to_s
+    elsif n < 1000000
+      return "#{n / 1000}.#{(n % 1000) / 100} K"
+    else
+      return "#{n / 1000000}.#{(n % 1000000) / 100000} M"
+    end
+  end
 end
